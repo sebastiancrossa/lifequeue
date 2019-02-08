0516d1ff9823724dd7b8d793a0937e3e3738c68f
Sebastian Crossa <crossasebastian@gmail.com>
2019-02-07 23:40:32 -0600

    Implemented routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 import uuid from 'uuid';
 
 // COMPONENTS
 import Todos from './components/Todos'
 import AddTodo from './components/AddTodo'
+import About from './components/pages/About'
 
 // Display functions
 import Header from './components/layout/Header'
@@ -74,13 +78,20 @@ class App extends Component {
   render() {
     console.log(this.state.todos);
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo} />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos} markComplete={this.markComplete} deleteTodo={this.deleteTodo} />
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
